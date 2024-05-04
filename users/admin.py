b09ab2626ff1a34b740e7e9bda0280df2bb6d6b3
medlcago <sasha.vol40@mail.ru>
2024-05-04 14:36:16 +0300

    Initial commit

---
+++ users/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Profile
+
+User = get_user_model()
+admin.site.unregister(User)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    save_on_top = True
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "full_name", "is_staff", "last_login", "date_joined")
+
+    @admin.display(description="Полное имя")
+    def full_name(self, obj):
+        return obj.first_name + " " + obj.last_name
---
+++ users/models/profile.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.db import models, transaction
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+User = get_user_model()
+
+
+def get_avatar_upload_path(instance, filename):
+    base_path = "avatars"
+    return f"{base_path}/{instance.user.username}/{filename}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile", verbose_name="Профиль")
+    avatar = models.ImageField(upload_to=get_avatar_upload_path, default=None, blank=True, null=True,
+                               verbose_name="Аватарка")
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+        ordering = ("pk",)
+
+    def __str__(self):
+        return f"Профиль {self.user}"
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_instance = Profile.objects.get(pk=self.pk)
+            if old_instance.avatar and self.avatar != old_instance.avatar:
+                old_instance.avatar.delete(save=False)
+        super().save(*args, **kwargs)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if not hasattr(instance, "profile"):
+        with transaction.atomic():
+            Profile.objects.create(user=instance)
