a0ee54695422b1500904e2cbe306ec2ef78a952f
medlcago <sasha.vol40@mail.ru>
2024-05-07 13:20:36 +0300

    sanitize user input

--- apps/posts/templatetags/post_tags.py
+++ apps/posts/templatetags/post_tags.py
@@ -1,3 +1,4 @@
+import bleach
 from django import template
 from django.utils.html import format_html
 
@@ -9,3 +10,10 @@ def publish_status(post):
     if post.is_published:
         return format_html('<i class="fas fa-check text-success" title="Вы являетесь автором"></i>')
     return format_html('<i class="fas fa-times text-danger" title="Черновик"></i>')
+
+
+@register.filter
+def sanitize_user_input(text):
+    allowed_tags = ["p", "b", "i", "u"]
+    cleaned_text = bleach.clean(text, tags=allowed_tags)
+    return format_html(cleaned_text)
