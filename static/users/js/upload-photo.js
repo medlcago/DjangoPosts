4df06159273eebeed98609db519a73f3927467e2
medlcago <sasha.vol40@mail.ru>
2024-05-14 08:22:53 +0300

    Minor fixes

--- static/users/js/upload-photo.js
+++ static/users/js/upload-photo.js
@@ -1,5 +1,6 @@
 $(".save-photo").click(function () {
     $("#failed-upload-photo").remove();
+    $("#unsupported-extension").remove();
     let input = $("#formFile")[0];
 
     if (input.files && input.files[0]) {
@@ -18,6 +19,8 @@ $(".save-photo").click(function () {
             },
             error: function (xhr, status, error) {
                 console.error("Ошибка при загрузке файла на сервер:", error);
+                let unsupportedExtensionMessage = $('<small class="text-danger" id="unsupported-extension">Невозможно обработать данное изображение</small>');
+                $(".upload-photo").after(unsupportedExtensionMessage);
             }
         });
         setTimeout(function () {
